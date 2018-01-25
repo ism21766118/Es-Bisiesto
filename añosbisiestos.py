ec17c0426a498d8f7609ce262f341fdc387ddd58
ism21766118 <ism21766118@a07.informatica.escoladeltreball.org>
2018-01-25 13:41:46 +0100

    first commit

--- añosbisiestos.py
+++ añosbisiestos.py
@@ -1,12 +1,14 @@
 #coding: utf-8
-anio=input("inserte su año aqui:")
-if anio%400==0:
+def esBisiesto(anio):
+
+ if anio%400==0:
   print " el año es bisiesto"
-elif anio%100==0:
-    print " el año no es bisiesto"
-elif anio%4==0:
-    print " el año es bisiesto"
-
-
-  
     
+ elif anio%100==0:
+    print " el año no es bisiesto"
+    
+ elif anio%4==0:
+    print " el año es bisiesto"
+    
+  
+anio=input("inserte su año aqui:")   
